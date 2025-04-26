cf23ea55e8d139944bf03f58714c6f5e270f1d7b
Selen Okbaz <selen.okbaz12@gmail.com>
2025-04-26 16:17:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidbootcampodev1/Odev1.kt
@@ -0,0 +1,145 @@
+package com.example.androidbootcampodev1
+
+fun main(){
+    var sehir="Antalya"
+    println("Şehir: $sehir")
+
+    var ulke="Türkiye"
+    println("Ülke: $ulke")
+
+    var telefon= 333333333
+    println("Telefon Numarası: $telefon")
+
+    var postaKodu= 7070
+    println("Posta Kodu: $postaKodu")
+
+    var email= "selen.okbaz12@gmail.com"
+    println("E-mail: $email")
+
+    var meslek="Bilgisayar Mühendisi"
+    println("Meslek: $meslek")
+
+    var stokMiktari=15268
+    println("Stok Miktarı: $stokMiktari")
+
+    val musteriAdi= "Zeytin"
+    println("Müşteri Adı: $musteriAdi")
+
+    var bakiye=20
+    println("Kalan Bakiye: $bakiye Türk Lirası" )
+
+    val dogumGunu= "2 Aralık 2002"
+    println("Doğum Günü: $dogumGunu")
+
+    var maas= 150000
+    println("Güncel Maaş: $maas")
+
+    var medeniDurum="Bekar"
+    println("Medeni Durumunuz: $medeniDurum")
+
+    var urunYorum="Güzel"
+    println("Ürün Yorumunu Giriniz: $urunYorum")
+
+    var odemeTarihi="2025-06-12"
+    println("Son Ödeme Tarihi: $odemeTarihi")
+
+    var odeme=500
+    println("Ödenmesi Gereken Türk Lirası Miktarı: $odeme")
+
+    var siparisAdeti=25
+    println("Sipariş Adeti: $siparisAdeti")
+
+    var arabaModeli="Renault"
+    println("Araba Modeli: $arabaModeli")
+
+    var kitapAdi="Dördüncü Kanat"
+    println("Kitap Adı: ")
+
+    var yayinlanmaTarihi="5 Nisan 2023"
+    println("Yayınlanma Tarihi: $yayinlanmaTarihi")
+
+    var indirimMiktari=100
+    println("İndirim Miktarı $indirimMiktari TL")
+
+    var odaSayisi=5
+    println("Oda Sayısı: $odaSayisi")
+
+    var enlem="36–42° Kuzey"
+    println("Türkiye'nin bulunduğu enlemler: $enlem ")
+
+    var boylam=" 26–45° Doğu"
+    println("Türkiye'nin bulunduğu boylamlar: $boylam")
+
+    var urunAdi="Cam Şato Serisi 2. Kitap"
+    println("Ürün Adı: $urunAdi")
+
+    var yemekFiyati=296
+    println("Menü Fiyat: $yemekFiyati")
+
+    var marka="Mavi"
+    println("Marka: $marka")
+
+    var muzikAdi="Diana"
+    println("Müzik: $muzikAdi")
+
+    var videoSuresi=3.00
+    println("Video Süresi: $videoSuresi ")
+
+    var urunPuani=2.5
+    println("Ortalama Ürün Puanı: $urunPuani ")
+
+    var resimAdi="image.png"
+    println("Resim Adı: $resimAdi")
+
+    var dosyaFormati=".pdf"
+    println("Gönderilmesi Gereken Dosyanın Formatı: $dosyaFormati")
+
+    var renk="Navy Blue"
+    println("Renk: $renk")
+
+    var renkKodu="#00003c"
+    println("Renk Kodu: $renkKodu")
+
+    var telefonModeli="IPhone 11"
+    println("Telefon Modeli: $telefonModeli")
+
+    var ekranBoyutu=6.1
+    println("Ekran Boyutu: $ekranBoyutu")
+
+    var agirlik=194
+    println("Ağırlık (gram): $agirlik")
+
+    var ulusalGun="23 Nisan Ulusal Egemenlik ve Çocuk Bayramı"
+    println("Türkiye’de her yıl kutlanan, Türkiye Büyük Millet Meclisi’nin açılışını simgeleyen ve dünya çocuklarına armağan edilen bayramın adı nedir? $ulusalGun")
+
+    var tatilGunu="Pazar"
+    println("Tatil Günümüz: $tatilGunu")
+
+    var rezervasyonTarihi="2025-07-28"
+    println("Rezervasyon Yapmak İstediğiniz Tarihi Giriniz: $rezervasyonTarihi")
+
+    var sokakAdi="1406. Sokak"
+    println("Sokak Adı: $sokakAdi")
+
+    var otobusHatti="Tıp Fakültesi-Bilim Üniversitesi"
+    println("AB07 Otobüs Hattı: $otobusHatti")
+
+    var kalanDakika="10 dk / 7 durak "
+    println("Otobüse Kalan Dakika: $kalanDakika")
+
+    var takipKodu="XY789KL012"
+    println("Kargo Takip Kodunuz: $takipKodu")
+
+    var kuponSuresi="15 gün"
+    println("Kuponun Kalan Süresi: $kuponSuresi")
+
+    var kuponKodu="ZYT100"
+    println("KUllanılabilir Kupon Kodu: $kuponKodu")
+
+    var faturaAdresi="Zeytin Mahallesi Pati Sokak Kuyruk Residence B blok kat:3 no:3"
+    println("Fatura Adresini Giriniz: $faturaAdresi")
+
+
+
+
+}
